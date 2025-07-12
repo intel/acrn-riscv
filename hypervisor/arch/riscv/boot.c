5743a7266260c9749ae230011518c4f756c4f2e5
Haicheng Li <haicheng.li@outlook.com>
2025-07-12 11:35:53 +0800

    riscv: adjust ACRN MEM layout
    Signed-off-by: Haicheng Li <haicheng.li@outlook.com>

--- hypervisor/arch/riscv/boot.c
+++ hypervisor/arch/riscv/boot.c
@@ -9,11 +9,11 @@
 #include <asm/boot.h>
 
 #ifndef CONFIG_EFI_BOOT
-#define KERNEL_IMAGE_START	0x81000000
-#define KERNEL_IMAGE_SIZE	0x10000000
-#define DTB_IMAGE_START		0x82200000
-#define DTB_IMAGE_SIZE		0x400000
 
+#define KERNEL_IMAGE_SIZE	0x10000000
+#define DTB_IMAGE_SIZE		CONFIG_SOS_DTB_SIZE
+
+#ifdef CONFIG_MACRN
 struct fw_dynamic_info *fw_dinfo = NULL;
 paddr_t fw_dtb = 0UL;
 
@@ -28,6 +28,22 @@ void get_dtb_info(struct dtb_info *info)
 	info->dtb_addr = fw_dtb;
 	info->dtb_len = DTB_IMAGE_SIZE;
 }
+#else
+#define KERNEL_IMAGE_START	CONFIG_SOS_MEM_START
+#define DTB_IMAGE_START		CONFIG_SOS_DTB_BASE
+
+void get_kernel_info(struct kernel_info *info)
+{
+	info->kernel_addr = KERNEL_IMAGE_START;
+	info->kernel_len = KERNEL_IMAGE_SIZE;
+}
+
+void get_dtb_info(struct dtb_info *info)
+{
+	info->dtb_addr = DTB_IMAGE_START;
+	info->dtb_len = DTB_IMAGE_SIZE;
+}
+#endif
 
 /* get the actual Hypervisor load address (HVA) */
 uint64_t get_hv_image_base(void)
