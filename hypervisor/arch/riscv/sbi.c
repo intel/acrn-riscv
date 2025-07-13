8c98857440c8ba78aefb4082aaf283bb7a7c50eb
Haicheng Li <haicheng.li@outlook.com>
2025-07-13 12:10:30 +0800

    riscv: add sbi platform base driver
    Signed-off-by: Haicheng Li <haicheng.li@outlook.com>

---
+++ hypervisor/arch/riscv/sbi.c
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2025 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <lib/types.h>
+#include <asm/lib/bits.h>
+#include <asm/cpu.h>
+#include <asm/per_cpu.h>
+#include <asm/smp.h>
+#include <asm/sbi.h>
+
+static sbi_ret sbi_ecall(uint64_t arg0, uint64_t arg1, uint64_t arg2,
+				uint64_t arg3, uint64_t arg4, uint64_t arg5,
+				uint64_t func, uint64_t ext)
+{
+	sbi_ret ret;
+
+	register uint64_t a0 asm ("a0") = arg0;
+	register uint64_t a1 asm ("a1") = arg1;
+	register uint64_t a2 asm ("a2") = arg2;
+	register uint64_t a3 asm ("a3") = arg3;
+	register uint64_t a4 asm ("a4") = arg4;
+	register uint64_t a5 asm ("a5") = arg5;
+	register uint64_t a6 asm ("a6") = func;
+	register uint64_t a7 asm ("a7") = ext;
+
+	asm volatile (
+		"ecall \n\t"
+		:"+r" (a0), "+r" (a1)
+		:"r" (a2), "r" (a3), "r" (a4), "r" (a5), "r" (a6), "r" (a7)
+		: "memory"
+	);
+
+	ret.error = a0;
+	ret.value = a1;
+
+	return ret;
+}
+
+static int do_swi(int cpu)
+{
+	uint64_t mask = 1UL << cpu;
+	sbi_ret ret;
+
+	ret = sbi_ecall(mask, 0, 0, 0, 0, 0, SBI_TYPE_IPI_SEND_IPI, SBI_ID_IPI);
+	if (ret.error != SBI_SUCCESS)
+		pr_err("%s: %lx", __func__, ret.error);
+
+	return ret.error;
+}
+
+static void send_single_swi(uint16_t pcpu_id, uint64_t vector)
+{
+	set_bit(vector, &per_cpu(swi_vector, pcpu_id).type);
+	do_swi(pcpu_id);
+}
+
+static void send_dest_ipi_mask(uint64_t dest_mask, uint64_t vector)
+{
+	uint16_t pcpu_id;
+	uint64_t mask = dest_mask;
+
+	pcpu_id = ffs64(mask);
+	if (pcpu_id--)
+		return;
+	while (pcpu_id < NR_CPUS) {
+		clear_bit(pcpu_id, &mask);
+		send_single_swi(pcpu_id, vector);
+		pcpu_id = ffs64(mask);
+	}
+}
+
+static int ipi_start_cpu(int cpu, uint64_t addr, uint64_t arg)
+{
+	sbi_ret ret;
+
+	ret = sbi_ecall((uint64_t)cpu, addr, arg, 0, 0, 0, SBI_TYPE_HSM_HART_START,
+			SBI_ID_HSM);
+	if (ret.error != SBI_SUCCESS)
+		pr_err("%s: %lx", __func__, ret.error);
+
+	return ret.error;
+}
+
+static struct smp_ops sbi_smp_ops =
+	{do_swi, send_single_swi, send_dest_ipi_mask, ipi_start_cpu};
+
+void init_sbi_ipi(void)
+{
+	register_smp_ops(&sbi_smp_ops);
+}
+
+static void sbi_preinit_timer(void)
+{
+	return;
+}
+
+static int sbi_set_deadline(uint64_t deadline)
+{
+	sbi_ret ret;
+
+	ret = sbi_ecall(deadline, 0, 0, 0, 0, 0, SBI_TYPE_TIME_SET_TIMER,
+			SBI_ID_TIMER);
+
+	return ret.error;
+}
+
+static uint64_t sbi_get_tick(void)
+{
+	uint64_t tick;
+	asm volatile (
+		"rdtime t0 \n\t"
+		"sw t0, 0(%0) \n\t"
+			::"r"(&tick): "memory", "t0");
+	return tick;
+}
+static struct timer_ops sbi_timer_ops =
+	{sbi_preinit_timer, sbi_get_tick, sbi_set_deadline};
+
+void init_sbi_timer(void)
+{
+	register_timer_ops(&sbi_timer_ops);
+}
---
+++ hypervisor/include/arch/riscv/asm/sbi.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2023-2024 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __RISCV_SBI_H__
+#define __RISCV_SBI_H__
+
+enum sbi_id {
+	SBI_ID_BASE = 0x10,
+	SBI_ID_TIMER = 0x54494D45,
+	SBI_ID_IPI = 0x735049,
+	SBI_ID_RFENCE = 0x52464E43,
+	SBI_ID_HSM = 0x48534D,
+	SBI_ID_SRST = 0x53525354,
+	SBI_ID_PMU = 0x504D55,
+	SBI_ID_MPXY = 0x4D505859,
+
+	/* Experimentals extensions must lie within this range */
+	SBI_EXPERIMENTAL_START = 0x08000000,
+	SBI_EXPERIMENTAL_END = 0x08FFFFFF,
+
+	/* Vendor extensions must lie within this range */
+	SBI_VENDOR_START = 0x09000000,
+	SBI_VENDOR_END = 0x09FFFFFF,
+};
+
+#define SBI_TYPE_BASE_GET_SPEC_VERSION		0x0
+#define SBI_TYPE_BASE_GET_IMP_ID		0x1
+#define SBI_TYPE_BASE_GET_IMP_VERSION		0x2
+#define SBI_TYPE_BASE_PROBE_EXT			0x3
+#define SBI_TYPE_BASE_GET_MVENDORID		0x4
+#define SBI_TYPE_BASE_GET_MARCHID		0x5
+#define SBI_TYPE_BASE_GET_MIMPID		0x6
+
+/* SBI function IDs for TIME extension*/
+#define SBI_TYPE_TIME_SET_TIMER			0x0
+
+/* SBI function IDs for IPI extension*/
+#define SBI_TYPE_IPI_SEND_IPI			0x0
+
+/* SBI function IDs for RFENCE extension*/
+#define SBI_TYPE_RFENCE_FNECE_I			0x0
+#define SBI_TYPE_RFENCE_SFNECE_VMA		0x1
+#define SBI_TYPE_RFENCE_SFNECE_VMA_ASID		0x2
+
+/* SBI function IDs for HSM extension*/
+#define SBI_TYPE_HSM_HART_START			0x0
+#define SBI_TYPE_HSM_HART_STOP			0x1
+#define SBI_TYPE_HSM_HART_GET_STATUS		0x2
+#define SBI_EXT_HSM_HART_SUSPEND		0x3
+
+/* SBI function IDs for MPXY extension*/
+#define SBI_TYPE_MPXY_GET_SHM_SIZE		0x0
+#define SBI_TYPE_MPXY_SET_SHM			0x1
+#define SBI_TYPE_MPXY_GET_CHANNEL_IDS		0x2
+#define SBI_TYPE_MPXY_READ_ATTRS		0x3
+#define SBI_TYPE_MPXY_WRITE_ATTRS		0x4
+#define SBI_TYPE_MPXY_SEND_MSG_WITH_RESP	0x5
+#define SBI_TYPE_MPXY_SEND_MSG_WITHOUT_RESP	0x6
+#define SBI_TYPE_MPXY_GET_NOTFICATION_EVENTS	0x7
+
+/* SBI return error codes */
+#define SBI_SUCCESS				0
+#define SBI_EFAILURE				-1
+#define SBI_ENOTSUPP				-2
+#define SBI_EINVAL_PARAM			-3
+#define SBI_EDENIED				-4
+#define SBI_EINVAL_ADDR				-5
+#define SBI_EAVAILABLE				-6
+#define SBI_ESTARTED				-7
+#define SBI_ESTOPPED				-8
+
+#define SBI_RFENCE_FLUSH_ALL ((uint64_t)-1)
+
+typedef struct {
+	int64_t error;
+	int64_t value;
+} sbi_ret;
+
+extern void init_sbi_ipi(void);
+extern void init_sbi_timer(void);
+
+#endif /*  __RISCV_SBI_H__ */
